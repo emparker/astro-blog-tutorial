df74dc863fbd86990d6938c883c715a3a0be2e8c
Emily Parker <emilyarparker@gmail.com>
2023-10-17 15:28:42 -0600

    Adds integrations: React, Tailwind, partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,22 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ["astro.build"]
   },
-  
   site: 'https://emilys-astro-blog.netlify.app',
-  integrations: [sitemap()],
+  integrations: [sitemap(), react(), tailwind(), partytown()]
   // if need
   // integrations: [sitemap({
   //   filter: (page) => !== "" ex=> to not include a robots.txt as meta tag. dont want indexed or visable// limiting posts
   // })],
-
 });
-
 
 // if need :
 //   // Define your pages configuration here
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,20 @@
 module.exports = {
-    plugins: [
-        require('postcss-preset-env')({
-            stage: 0,
-        })
-    ]
-}
+    plugins: {
+      'postcss-import': {},
+      'tailwindcss/nesting': 'postcss-nesting',
+      tailwindcss: {},
+      'postcss-preset-env': {
+        features: { 'nesting-rules': false },
+      },
+    }
+  }
+
+// previous
+// module.exports = {
+//     plugins: [
+//         require('postcss-preset-env')({
+//             stage: 0,
+//         })
+
+//     ]
+// }
